7ff84825c156e75970942b6784ce2dea358504f3
LucasG <lks.gauna@gmail.com>
2025-05-19 15:40:07 -0300

    add: simple test

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from main import app
+
+cliente = TestClient(app)
+def test_hola_mundo():
+    response = cliente.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hola mundo"}
+
+
+
