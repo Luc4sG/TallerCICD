765b8f7f0f33c624a3d3fbc4b712ee7bff74ef83
Lucas Gauna <lks.gauna@gmail.com>
2025-05-20 19:57:14 -0300

    test: should pass v2

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import app
 cliente = TestClient(app)
 def test_hola_mundo():
     response = cliente.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 200  
     assert response.json() == {"message": "Hola mundo"}
 
 
